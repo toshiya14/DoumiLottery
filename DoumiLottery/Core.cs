573f6969c8add6ad51075fb8ea1a09d499c53272
Toshiya14 <Toshiya14@live.jp>
2017-06-28 17:56:19 +0800

    Initialize Project.

---
+++ DoumiLottery/Core.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoumiLottery
+{
+    public static class Core
+    {
+        static Core() {
+            IsFinishedReading = false;
+            random = new Random((int)(DateTime.Now.Ticks));
+        }
+
+        private static Random random;
+        private static ISet<string> samples;
+        private static IList<string> results;
+        public static bool IsFinishedReading {
+            get;
+            private set;
+        }
+
+        public static async Task<ISet<string>> ReadSamplesFromFile(string filename) {
+            var sampleSet = new HashSet<string>();
+            var fs = new FileStream(filename, FileMode.Open);
+            using (var reader = new StreamReader(fs)) {
+                while (true) {
+                    if (reader.EndOfStream) {
+                        break;
+                    }
+
+                    var text = await reader.ReadLineAsync();
+
+                    if (string.IsNullOrWhiteSpace(text)) {
+                        continue;
+                    }
+
+                    sampleSet.Add(text);
+                }
+            }
+            IsFinishedReading = true;
+            return sampleSet;
+        }
+
+        public static async Task WriteResult(string filename) {
+            var fs = new FileStream(filename, FileMode.Append);
+            using (var writer = new StreamWriter(fs)) {
+                foreach (var i in results) {
+                    await writer.WriteLineAsync(i);
+                }
+            }
+        }
+
+        public static string[] Draw(ISet<string> samples, int count) {
+            List<string> _samples = new List<string>();
+            List<string> result = new List<string>();
+            _samples.AddRange(samples);
+            Shuffle(_samples, random.Next());
+            return _samples.Take(count).ToArray();
+        }
+
+        public static void Shuffle(IList<string> samples, int times) {
+            var sampleCount = samples.Count();
+            for (var i = 0; i < times; i++) {
+                var index1 = random.Next(0, sampleCount);
+                var index2 = random.Next(0, sampleCount);
+                var temp = samples[index1];
+                samples[index1] = samples[index2];
+                samples[index2] = temp;
+            }
+        }
+
+        public static void Remove(ISet<string> samples, string value) {
+            samples.Remove(value);
+        }
+
+        public static void Remove(ISet<string> samples, ICollection<string> value) {
+            foreach (var i in value) {
+                samples.Remove(i);
+            }
+        }
+
+        public static KeyValuePair<string, string> Mask(string input) {
+            var key = input;
+            var value = input.Substring(0, 3) + "****" + input.Substring(7);
+            return new KeyValuePair<string, string>(key, value);
+        }
+    }
+}
---
+++ DoumiLottery/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace DoumiLottery
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow :Window
+    {
+
+
+        DispatcherTimer timer;
+        string status;
+        Task t_Reading, t_Writing;
+
+        bool IsReadingCompleted {
+            get => Core.IsFinishedReading;
+        }
+
+        public MainWindow() {
+            InitializeComponent();
+
+            // Initialize Timer
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 200);
+            timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e) {
+            if (!IsReadingCompleted) {
+                return;
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+            timer.Start();
+            status = "正在读取样本列表...";
+            t_Reading = Core.ReadSamplesFromFile(@"./samples.txt");
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e) {
+            status = "等待结果写入完毕...";
+            if (t_Writing != null) {
+                while (!t_Writing.IsCompleted) {
+                    Task.Delay(100);
+                }
+            }
+            status = "正常退出";
+        }
+    }
+}
